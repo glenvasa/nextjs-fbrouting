6e17f60e728c4db692a115288bc49834b6148846
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-16 12:16:29 -0500

    Update [projectid].js

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,13 @@
+import { useRouter} from 'next/router'
+
 function PortfolioProjectPage(){
+    const router = useRouter()
+    console.log(router.pathname);
+    console.log(router.query);
+
+    // we could send a request to a backend server
+    // to fetch the piece of data with an id of router.query.projectid
+
     return <div>
         <h1>The PortfolioProjectPage</h1>
     </div>
