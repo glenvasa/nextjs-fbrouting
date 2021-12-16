e1a0b858aea6b1161749cf6e914a84436ecfc1e6
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-12-16 13:23:55 -0500

    created dynamic Links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,21 @@
+import Link from 'next/link'
+
 function ClientsPage(){
+
+    const clients = [
+        { id: 'glen', name: 'Glen'},
+        { id: 'nate', name: 'Nate'}
+    ]
+
     return <div>
         <h1>The Clients Page</h1>
+        <ul>
+            {clients.map(client => 
+            <li key={client.id}>
+                <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                </li>
+            )}
+        </ul>
     </div>
 }
 
